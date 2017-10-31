8a68566a140c8cc2d6799f6b8ad3ed53cb2606f4
wz2cool <wz2cool@live.cn>
2017-10-31 23:07:49 +0800

    add

---
+++ constants/initFlag.go
@@ -0,0 +1,10 @@
+package constants
+
+// DbTypeFlag for database type. (eg: mysql, sqlite)
+const DbTypeFlag = "dbtype"
+
+// FileFlag for creating table sql.
+const FileFlag = "file"
+
+// OutDirFlag (Output Directory) for entity files.
+const OutDirFlag = "outdir"
--- main/main.go
+++ main/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/alecthomas/log4go"
 )
 
 func main() {
 	fmt.Println("hello world")
+}
+
+func readCommandArgs() {
+	methodName := "readCommandArgs"
+	log4go.Info("%s start", methodName)
 }
