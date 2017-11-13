3b3f4038759c8d0f82a7ff4711dc855aa21271b9
wz2cool <wz2cool@live.cn>
2017-11-13 17:53:36 +0800

    add

--- src/public/js/controllers/entitygenerator/entityGeneratorController.js
+++ src/public/js/controllers/entitygenerator/entityGeneratorController.js
@@ -1,6 +1,20 @@
 (function () {
     'use strict';
-    angular.module('myApp').controller('entityGeneratorController', ['$scope', function ($scope) {
-        $scope.title = 'hello world';
-    }]);
+    angular.module('myApp').controller('entityGeneratorController',
+        ['$scope', '$uibModal', function ($scope, $uibModal) {
+
+
+            $scope.connect = function () {
+                $uibModal.open({
+                    animation: true,
+                    ariaLabelledBy: 'modal-title-top',
+                    ariaDescribedBy: 'modal-body-top',
+                    templateUrl: 'myModalContent.html',
+                    size: 'sm',
+                    controller: function ($scope) {
+                        $scope.name = 'top';
+                    }
+                });
+            }
+        }]);
 })();
