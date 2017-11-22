1f6933722e761a7c846fe081aaa9ee5fc0fd6fb5
wz2cool <wz2cool@live.cn>
2017-11-22 11:02:52 +0800

    add

--- src/helpers/templateHelper.ts
+++ src/helpers/templateHelper.ts
@@ -1,19 +1,20 @@
-import { ColumnInfo } from "tsbatis";
 import * as lodash from "lodash";
+import { ColumnInfo } from "tsbatis";
 
 export class TemplateHelper {
     public static generateTableEntity(tableName: string, columnInfos: ColumnInfo[]): string {
         const entityNameTemp = lodash.camelCase(tableName);
-        const entityName = entityNameTemp.substring(0, 1).toUpperCase() + entityNameTemp.substring(1, entityNameTemp.length);
+        const entityName = entityNameTemp.substring(0, 1).toUpperCase() +
+            entityNameTemp.substring(1, entityNameTemp.length);
         const properties = TemplateHelper.generateProperties(columnInfos);
         const result =
             `import { column, TableEntity } from "tsbatis"\r\n` +
             `\r\n` +
             `export class ${entityName} extends TableEntity {\r\n` +
             `${properties}\r\n` +
-            `   public getTableName(): string {\r\n` +
-            `       return "${tableName}";\r\n` +
-            `   }\r\n` +
+            `    public getTableName(): string {\r\n` +
+            `        return "${tableName}";\r\n` +
+            `    }\r\n` +
             `}\r\n`;
         return result;
     }
@@ -28,8 +29,8 @@ export class TemplateHelper {
 
     public static generateProperty(columnInfo: ColumnInfo): string {
         const result =
-            `   @column("${columnInfo.columnName}", ${columnInfo.isKey}, ${columnInfo.insertable})\r\n` +
-            `   public ${columnInfo.property}: ${columnInfo.propertyType};\r\n`;
+            `    @column("${columnInfo.columnName}", ${columnInfo.isKey}, ${columnInfo.insertable})\r\n` +
+            `    public ${columnInfo.property}: ${columnInfo.propertyType};\r\n`;
         return result;
     }
 }
